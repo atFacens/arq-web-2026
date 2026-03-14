3b757ffb5ca4debf47a1aa55f4482e2014a1fcf8
Emerson Paduan <emerson@paduan.dev.br>
2026-03-13 22:55:04 -0300

    aula 03

---
+++ aula03/proj01/src/main/java/br/facens/proj01/HelloController.java
@@ -0,0 +1,13 @@
+package br.facens.proj01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Boa noite!";
+    }
+}
